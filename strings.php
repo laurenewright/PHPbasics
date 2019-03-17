d58739a855149dfa6013172928e8d0cd1077bc27
lauren wright <wright.lauren20@gmail.com>
2019-03-17 13:38:37 -0400

    finished strings module

--- strings.php
+++ strings.php
@@ -1,6 +1,6 @@
 <?php
 $name = "Jack";
-$string_one = "Leanring to display \"Hello $name!\" to the screen. \n";
+$string_one = "Learning to display \"Hello $name!\" to the screen. \n";
 echo $string_one;
 
 $isReady = true;
@@ -11,6 +11,16 @@ var_dump(1 + "2");
 $a = 10;
 $b = '10';
 
-var_dump($a == $b);
+//var_dump($a == $b);
+//var_dump($a === $b);
+
+if($string_one == 'Learning to display "Hello Jack!" to the screen.'){
+  echo 'the values match';
+} elseif($string_one == ""){
+  echo '$string_one is empty';
+}
+  else {
+  echo 'the values do not match';
+}
 
 ?>
